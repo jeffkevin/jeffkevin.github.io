664b434a0e5b49e14e591ec23663d90c9521d7cd
jeffkevin <848949+jeffkevin@users.noreply.github.com>
2023-04-14 13:59:50 +0800

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jk-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'jeffkevin';
+}
